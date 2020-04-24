d681d12cf26083d9d8aa7d68d84c6967f66c4d0c
Ramon Abreu <53264058+ramonabreu-cdev@users.noreply.github.com>
2020-04-24 01:16:32 -0300

    Add files via upload

---
+++ dart/3_maps/01_maps.dart
@@ -0,0 +1,17 @@
+void main(){
+ //uma lista que recupera os valores por uma String 
+
+ var maps = {
+   "nome" : "Ramon",
+   "idade": 25,
+   "cidade": "Teresina",
+   "estado": "Piauí",
+ };
+
+ //pra acessar
+ 
+ print(maps["nome"]);
+
+ print(maps);
+
+}
---
+++ dart/3_maps/02_tiposMaps.dart
@@ -0,0 +1,17 @@
+void main(){
+ //uma lista que recupera os valores por uma String 
+
+ map<String, dynamic> maps = {
+   "nome" : "Ramon",
+   "idade": 25,
+   "cidade": "Teresina",
+   "estado": "Piauí",
+ };
+
+ //pra acessar
+ 
+ print(maps["nome"]);
+
+ print(maps);
+
+}
