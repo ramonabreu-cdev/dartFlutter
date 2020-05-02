8fef0c8b25e6bc979546165c0814960fd17ee647
Ramon Abreu <53264058+ramonabreu-cdev@users.noreply.github.com>
2020-05-02 19:44:13 -0300

    Add files via upload

---
+++ dart/4_poo/objetos/src/main.dart
@@ -0,0 +1,13 @@
+
+import 'pessoa.dart';
+
+main(List<String> arguments){
+  Pessoa pessoa = Pessoa("Ramon",25,"Masculino");
+  
+  print(pessoa.nome);
+  print(pessoa.idade);
+  print(pessoa.sexo);
+  
+  }
+  
+  
---
+++ dart/4_poo/objetos/src/pessoa.dart
@@ -0,0 +1,15 @@
+class Pessoa{
+
+  String nome;
+  int idade;
+  String sexo;
+
+  Pessoa(String nome, int idade,String sexo){
+
+
+    this.nome = nome;
+    this.idade = idade;
+    this.sexo = sexo;
+  }
+
+}
