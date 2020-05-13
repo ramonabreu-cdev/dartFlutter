c865b763f5a0d0a9ae41339afb31b8b4b6bc7bb0
Ramon Abreu <53264058+ramonabreu-cdev@users.noreply.github.com>
2020-05-13 06:39:23 -0300

    Add files via upload

---
+++ dart/4_poo/classe.dart
@@ -0,0 +1,15 @@
+//classe
+class Data{
+  int dia;
+  int mes;
+  int ano;
+}  
+void main(){
+  //objeto
+  var dataNiver = new Data();
+  dataNiver.dia = 1;
+  dataNiver.mes = 1;
+  dataNiver.ano = 1995;
+
+  print(dataNiver);
+}
